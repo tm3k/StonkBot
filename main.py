e72e48efe9877eee97c79621d40565a52bdeb3bd
tm3k <60632198+tm3k@users.noreply.github.com>
2020-04-11 16:16:31 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import pandas as pd
 # Creates API object
 api = tradeapi.REST(creds.api_key, creds.api_secret, api_version='v1') 
 
+
 # Can only call 100 stocks at a time thru the api so had to split it up
 set1 = spx500.spx[:100]
 set2 = spx500.spx[101:201]
@@ -20,26 +21,53 @@ barsets4 = api.get_barset(set1, timeframe = '1D', limit = 30)
 barsets5 = api.get_barset(set1, timeframe = '1D', limit = 30)
 
 
-# Method for getting ohlc data for the stock and day specified
+
+# Method for getting ohlc data for the stock and 30 day period
 def get_ohlc_data(stock,day):
     day_open = stock[day].o
     day_high = stock[day].h 
     day_low = stock[day].l 
-    day_close = stock[day].c 
+    day_close = stock[day].c
+    candle_time = stock[day].t 
 
-    print(day_open)
-    print(day_high)
-    print(day_low)
-    print(day_close)
+    
+    # print(f'Time:  {candle_time}')
+    # print(f'Open:  {day_open}')
+    # print(f'High:  {day_high}')
+    # print(f'Low:   {day_low}')
+    # print(f'Close: {day_close}')
+    
+    add_to_df(day_open, day_high, day_low, day_close,candle_time)
+
+
+# Method with lists to hold ohlc vals
+def add_to_df(day_open,day_high,day_low,day_close,candle_time):
+    
+    open_vals = []
+    high_vals = []
+    low_vals = []
+    close_vals = []
+
+    open_vals.append(day_open)
+    high_vals.append(day_high)
+    low_vals.append(day_low)
+    close_vals.append(day_close)
+
+    zippedList = list(zip(open_vals, high_vals, low_vals, close_vals))
+    df = pd.DataFrame(zippedList, columns = ['open' , 'high', 'low', 'close'])
+    df['time'] = candle_time
+    
+    
+
+    # Magic code to remove hours minutes and seconds from timestamp, formats code for viewing
+    df['time'] = pd.to_datetime(df['time'])
+    df['time'] = df['time'].dt.date
+    pd.set_option('display.width', None)
+    pd.set_option('display.max_rows', 30)
+    print(df)
+
+
 
 # Test code for calling get_ohlc_data and getting the first(of 30) day's ohlc data
 aapl_bars=barsets1['AAPL']
 get_ohlc_data(aapl_bars,0)
-
-# Method for passing each individual stock into get_ohlc_method through using a loop
-
-
-
-# Method for taking stock ohlc data and creating pandas database
-
-
