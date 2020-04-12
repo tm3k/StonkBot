686091cbe6acc8ee7f74876b93f5c53cca35bb83
tm3k <60632198+tm3k@users.noreply.github.com>
2020-04-12 12:19:32 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import creds
-import finta
+from finta import TA
 import alpaca_trade_api as tradeapi
 import spx500
 import pandas as pd
+import numpy as np
 
 # Creates API object
 api = tradeapi.REST(creds.api_key, creds.api_secret, api_version='v1') 
@@ -14,60 +15,88 @@ set2 = spx500.spx[101:201]
 set3 = spx500.spx[202:302]
 set4 = spx500.spx[303:403]
 set5 = spx500.spx[404:504]
-barsets1 = api.get_barset(set1, timeframe = '1D', limit = 30)
-barsets2 = api.get_barset(set1, timeframe = '1D', limit = 30)
-barsets3 = api.get_barset(set1, timeframe = '1D', limit = 30)
-barsets4 = api.get_barset(set1, timeframe = '1D', limit = 30)
-barsets5 = api.get_barset(set1, timeframe = '1D', limit = 30)
+barsets1 = api.get_barset(set1, timeframe = '1D', limit = 60)
+barsets2 = api.get_barset(set1, timeframe = '1D', limit = 60)
+barsets3 = api.get_barset(set1, timeframe = '1D', limit = 60)
+barsets4 = api.get_barset(set1, timeframe = '1D', limit = 60)
+barsets5 = api.get_barset(set1, timeframe = '1D', limit = 60)
 
 
 
-# Method for getting ohlc data for the stock and 30 day period
-def get_ohlc_data(stock,day):
-    day_open = stock[day].o
-    day_high = stock[day].h 
-    day_low = stock[day].l 
-    day_close = stock[day].c
-    candle_time = stock[day].t 
-
-    
-    # print(f'Time:  {candle_time}')
-    # print(f'Open:  {day_open}')
-    # print(f'High:  {day_high}')
-    # print(f'Low:   {day_low}')
-    # print(f'Close: {day_close}')
-    
-    add_to_df(day_open, day_high, day_low, day_close,candle_time)
-
-
-# Method with lists to hold ohlc vals
-def add_to_df(day_open,day_high,day_low,day_close,candle_time):
-    
+# Method for getting ohlc data for the stock 30 days at a time
+def prep_data(stock):
     open_vals = []
-    high_vals = []
-    low_vals = []
-    close_vals = []
+    for i in stock:
+        day_open = i.o
+        open_vals.append(day_open)
 
-    open_vals.append(day_open)
-    high_vals.append(day_high)
-    low_vals.append(day_low)
-    close_vals.append(day_close)
+    high_vals = []
+    for i in stock:
+        day_high = i.h
+        high_vals.append(day_high)
+
+    low_vals = []
+    for i in stock:
+        day_low = i.l
+        low_vals.append(day_low)
+
+    close_vals = []
+    for i in stock:
+        day_close = i.c
+        close_vals.append(day_close)
+
+
+    candle_times = []
+    for i in stock:
+        candle_time = i.t 
+        candle_times.append(candle_time)
+
 
     zippedList = list(zip(open_vals, high_vals, low_vals, close_vals))
     df = pd.DataFrame(zippedList, columns = ['open' , 'high', 'low', 'close'])
-    df['time'] = candle_time
-    
-    
+    df['time'] = candle_times
 
     # Magic code to remove hours minutes and seconds from timestamp, formats code for viewing
     df['time'] = pd.to_datetime(df['time'])
     df['time'] = df['time'].dt.date
-    pd.set_option('display.width', None)
-    pd.set_option('display.max_rows', 30)
-    print(df)
+    
+    bb = TA.PERCENT_B(df)
+    bb = np.nan_to_num(bb)
+    df["%BB"] = bb
+    
 
+    trade_signal = []
+    for i in bb:
+        
+        if i == 0:
+            trade_signal.append(''),
+        elif i > 1:
+            trade_signal.append('X'),
+        elif i < 0:
+            trade_signal.append('X'),
+        elif i <= 1 and i >= 0:
+            trade_signal.append('')
+
+    action = pd.DataFrame(trade_signal)
+    df['Trade'] = action
+
+    pd.set_option('display.width', None)
+    pd.set_option('display.max_rows', 60)
+    return df
+    
+
+
+    
 
 
 # Test code for calling get_ohlc_data and getting the first(of 30) day's ohlc data
 aapl_bars=barsets1['AAPL']
-get_ohlc_data(aapl_bars,0)
+AAPL = prep_data(aapl_bars)
+
+print(AAPL)
+
+
+
+
+
+
